e51768e3ba3a3bcbdb920836ebc551fad9170d22
Rizza Marwan <rizzamarwan@icloud.com>
2025-01-09 23:18:00 +0700

    Update
    Added intro.html

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const bodyEl = document.body
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("#site-header")
+
+    const intercept = document.createElement("div")
+    intercept.dataset.observerIntercept = ""
+    header.before(intercept)
+
+    const headerObserver = new IntersectionObserver(([entry]) => {
+        header.classList.toggle("active", !entry.isIntersecting)
+    })
+    headerObserver.observe(intercept)
+})
